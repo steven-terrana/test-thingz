ce811ece27cf1674f41fec63a2b20439a7b8830b
Steven Terrana <steventerrana@gmail.com>
2023-04-28 11:23:53 -0400

    oops

--- jobs/create_multibranch.groovy
+++ jobs/create_multibranch.groovy
@@ -4,6 +4,8 @@ import org.yaml.snakeyaml.Yaml
 def workDir = SEED_JOB.getWorkspace()
 def config = new Yaml().load(("${workDir}/jobs.yaml" as File).text)
 
-config.folders.each{ folder ->
-  folder(folder)
-}
+println config
+
+// config.folders.each{ folder ->
+//   folder(folder)
+// }
