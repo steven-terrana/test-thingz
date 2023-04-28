1d6e940c9ff1a5111e607cfa060e4bf95b8fcbf9
Steven Terrana <steventerrana@gmail.com>
2023-04-28 11:32:40 -0400

    oops

--- jobs/create_multibranch.groovy
+++ jobs/create_multibranch.groovy
@@ -6,7 +6,9 @@ def config = new Yaml().load(("${workDir}/jobs.yaml" as File).text)
 
 config.systems.each{ system -> 
   folder(system.name)
-  system.components.each{ c ->
-      job("${system.name}/${c.name}")  
+  system.components.each{ component ->
+    multibranchPipelineJob("${system.name}/${component.name}") {
+
+    }
   }
 }
